5e40f2f8af5ff5bee3e62d09662710a001837675
Administrator <admin@gcpgitlab.tk>
2018-09-10 19:12:34 +0500

    created document model

---
+++ models/doc.js
@@ -0,0 +1,44 @@
+//Create Schema
+const DocSchema = new Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    body:{
+        type: String,
+        required: true
+    },
+    status:{
+        type: String,
+        default: 'public'
+    },
+    allowComments:{
+        type: Boolean,
+        default: true
+    },
+    comments:[{
+        commentBody:{
+            type: String
+        },
+        commentDate:{
+            type: Date,
+            default: Date.now
+        },
+        commentUser:{
+            type: Schema.Types.ObjectId,
+            ref:'users'
+        }
+    }],
+    user:{
+        type: Schema.Types.ObjectId,
+        ref:'users'
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+//Create collection and add schema
+mongoose.model('stories', StorySchema, 'stories');
