19f13e6573dd80125c3086470b435e4c5a322592
Mohamed Ziyau <34277561+xiyau@users.noreply.github.com>
2018-10-27 16:10:23 +0500

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports ={
-    mongoURI:'mongodb://hackode:Sndvol32@ds249942.mlab.com:49942/docs-refs',
-    googleClinetID:'859829585458-ql6k2813ellm333fbb3lm6jtrjqmnhlv.apps.googleusercontent.com',
-    googleClientSecret: 'rKsZJSJVME-9qPQG1Lx_GjH_'
+    mongoURI:'',
+    googleClinetID:'',
+    googleClientSecret: ''
 }
